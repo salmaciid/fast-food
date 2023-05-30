95b8e31ccf211509f34784e72e6dedfe27964dbb
salmaciid <salmaciid44@gmail.com>
2023-05-30 12:18:06 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import HeadlineCard from './components/HeadlineCard'
+import Food from './components/Food'
+import Category from './components/Category'
+
+
+function App() {
+
+
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        <HeadlineCard/>
+        <Food/>
+       <Category />
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/components/HeadlineCard.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+
+const HeadlineCards = () => {
+  return (
+    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+          <p className='px-2'>Through 8/26</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.pexels.com/photos/1199957/pexels-photo-1199957.jpeg?auto=compress&cs=tinysrgb&w=600'
+          alt='/'
+        />
+      </div>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>New Restaurants</p>
+          <p className='px-2'>Added Daily</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.unsplash.com/photo-1544025162-d76694265947?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTV8fGJicXxlbnwwfDB8MHx8&auto=format&fit=crop&w=800&q=60'
+          alt='/'
+        />
+      </div>
+      {/* Card */}
+      <div className='rounded-xl relative'>
+        {/* Overlay */}
+        <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+          <p className='font-bold text-2xl px-2 pt-4'>We Deliver Desserts Too</p>
+          <p className='px-2'>Tasty Treats</p>
+          <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+        </div>
+        <img
+        className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl'
+          src='https://images.pexels.com/photos/1653877/pexels-photo-1653877.jpeg?auto=compress&cs=tinysrgb&w=600'
+          alt='/'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default HeadlineCards;
